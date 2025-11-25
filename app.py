51d995c04d64231a4af2ca3dd52135061012ebdb
Adami-j <71751140+Adami-j@users.noreply.github.com>
2025-11-25 19:12:24 +0100

    prep online

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
-
+import random
+import re
 from core.model_loader import ModelLoader
 from core.game_engine import CemantixEngine
 
@@ -13,8 +14,24 @@ model = loader.load()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+
+def get_simple_random_word(model):
+    vocab = list(model.key_to_index.keys())
+
+    # Filtre : mots simples uniquement
+    simple_words = [
+        w for w in vocab
+        if 4 <= len(w) <= 8                 # mots courts
+        and re.fullmatch(r"[a-zàâçéèêëîïôûùüÿñæœ]+", w)  # pas de caractères bizarres
+    ]
+
+    return random.choice(simple_words)
+
+random_word = get_simple_random_word(model)
 engine = CemantixEngine(model)
-engine.new_game("chien")  # mot du jour
+engine.new_game(random_word)
+print(random_word)
+
 
 @app.get("/", response_class=HTMLResponse)
 def home():
@@ -26,4 +43,4 @@ def guess(word: str):
     return engine.guess(word)
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=1256, reload=True)

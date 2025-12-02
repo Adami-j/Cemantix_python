afc6e55d5f5317ac0193d4c05ab6ad06b9cedafa
AlexiRn3 <alexi.mathiou@icloud.com>
2025-12-02 11:21:13 +0100

    fix button index pages

--- static/js/main.js
+++ static/js/main.js
@@ -815,6 +815,8 @@ async function copyToClipboard(text) {
 export function initApp() {
     console.log("Initialisation de l'application...");
 
+    injectBugButton();
+
     // 1. Mise à jour des références DOM
     elements.form = document.getElementById("guess-form");
     elements.input = document.getElementById("word-input");
@@ -1247,7 +1249,6 @@ if (chatForm) {
 }
 
 function injectBugButton() {
-    // Vérifie si le bouton existe déjà pour éviter les doublons
     if (document.getElementById('bug-trigger')) return;
 
     const btn = document.createElement('button');
